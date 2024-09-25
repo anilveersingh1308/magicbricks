4f90cc6120fdf89c430e001855a8658baf47333a
Anil Singh <anilveersinghstudycode@gmail.com>
2024-09-25 21:26:11 +0530

    Html content present at navbar changed to react

--- src/components/Body/SearchBar.jsx
+++ src/components/Body/SearchBar.jsx
@@ -9,38 +9,44 @@ import { IoSearch } from 'react-icons/io5'
 
 
 export const SearchBar = () => {
+    const desc1 = [{head: 'Find a home you\'ll', head2: 'Love'}];
+    const desc2 = [{prop_header: 'We\'ve got properties for everyone', pop_header: 'Popular Owner Properties'}];
     return (
         <>
+            
             <div className="body-container-model">
-                <div className="search-container-main-model">
-                    <div className="container1">
-                        <div className='container1-descTop'>Find a home you'll  <span>Love</span> </div>                
-                        {search_Menu.map((menu, menu_index) => (
-                            <div className="container-menu">
-                                <li key={menu_index}><a href="#"> {menu} </a></li>
+                {desc1.map((desc) => (
+                    <div className="search-container-main-model">
+                        <div className="container1">
+                            <div className='container1-descTop'>{desc.head}  <span>{desc.head2}</span> </div>                
+                            {search_Menu.map((menu, menu_index) => (
+                                <div className="container-menu">
+                                    <li key={menu_index}><a href="#"> {menu} </a></li>
+                                </div>
+                            ))}
+                        </div>
+                        
+                        <div className="body1-cotainer-serchbar">
+                            <div className="searchbar-container-location-finder searchbar-container"> 
+                                <PiMapPinFill className='search-marker' />
+                                <input type="text" placeholder='Add more' className='outlined-address' onKeyUp='' />
                             </div>
-                        ))}
-                    </div>
-                    
-                    <div className="body1-cotainer-serchbar">
-                        <div className="searchbar-container-location-finder searchbar-container"> 
-                            <PiMapPinFill className='search-marker' />
-                            <input type="text" placeholder='Add more' className='outlined-address' onKeyUp='' />
-                        </div>
-                        <div className="searchbar-container-type-finder searchbar-container">
-                            <AiFillHome className='type-icon'/>
-                            <span>Flat +1</span> <li><FaChevronDown /></li>
-                        </div>
-                        <div className="searchbar-container-budget-finder searchbar-container">
-                            <RiMoneyRupeeCircleFill className='budget-icon'/>
-                            <span>Budget</span> <li><FaChevronDown /></li>
-                        </div>
-                        <div className="searchbar-container-button searchbar-container">
-                            <IoSearch className='search-icon'/>
-                            <span>Search</span>
+                            <div className="searchbar-container-type-finder searchbar-container">
+                                <AiFillHome className='type-icon'/>
+                                <span>Flat +1</span> <li><FaChevronDown /></li>
+                            </div>
+                            <div className="searchbar-container-budget-finder searchbar-container">
+                                <RiMoneyRupeeCircleFill className='budget-icon'/>
+                                <span>Budget</span> <li><FaChevronDown /></li>
+                            </div>
+                            <div className="searchbar-container-button searchbar-container">
+                                <IoSearch className='search-icon'/>
+                                <span>Search</span>
+                            </div>
                         </div>
                     </div>
-                </div>
+                ))}
+                
                 <div className="image-preview-container">
                     {img_preview.map((image, index) => (
                         <div className="preview-outline" key={index}>
@@ -51,10 +57,13 @@ export const SearchBar = () => {
             </div>    
             <div className="body-general-properties">
                 <div className="general-properties-header">
-                    <div className="properties-header-title">
-                        <div className="header-title">We've got properties for everyone</div>
-                        <div className='outline'></div>
-                    </div>
+                    {desc2.map((desc) => (
+                        <div className="properties-header-title">
+                            <div className="header-title">{desc.prop_header}</div>
+                            <div className='outline'></div>
+                        </div>
+                    ))}
+                    
                     <div className="geaneral-properties-container">
                         {general_properties.map((general, index) => (
                             <div className="properties-container-type" key={index}>
@@ -66,10 +75,12 @@ export const SearchBar = () => {
                 </div>
             </div>
             <div className="body-properties-label">
-                <div className="properties-label-title">
-                    <div className="label-header-title">Popular Owner Properties</div>
-                    <div className='outline'></div>
-                </div>
+                {desc2.map((desc) => (
+                    <div className="properties-label-title">
+                        <div className="label-header-title">{desc.pop_header}</div>
+                        <div className='outline'></div>
+                    </div>
+                ))}
                 <div className="properties-label-content">
                     <div className="label-header-image">
                         <img src="" alt="" />
--- src/components/LinksArray.jsx
+++ src/components/LinksArray.jsx
@@ -142,3 +142,277 @@ export const links = [
         ],
     },
 ];
+
+export const buy_options = [
+    {
+        Head: 'Popular Choices',
+        sublink: [
+            {name: 'Ready to move', link: '/'},
+            {name: 'Owner Properties', link: '/'},
+            {name: 'Budget Homes', link: '/'},
+            {name: 'Premium Homes', link: '/'},
+            {name: 'New Projects', sName: 'new', link: '/'},
+        ],
+    },
+    {
+        Head: 'Properties Types',
+        sublink: [
+            {name: 'Flats in New-Delhi', link: '/'},
+            {name: 'House for sale in New...', link: '/'},
+            {name: 'Villa in New-Delhi', link: '/'},
+            {name: 'Plots in New-Delhi', link: '/'},
+            {name: 'Office in New_Delhi', link: '/'},
+            {name: 'Commercial Space in ...', link: '/'},
+        ],
+    },
+    {
+        Head: 'Budget',
+        sublink: [
+            {name: 'Under ₹ 50 Lac', link: '/'},
+            {name: '₹ 50 Lac - ₹ 1 Cr', link: '/'},
+            {name: '₹ 1 Cr - ₹ 1.5 Cr', link: '/'},
+            {name: 'Above ₹ 1.5 Cr', link: '/'},
+        ],
+    },
+    {
+        Head: 'Explore',
+        sublink: [
+            {name: 'Lacalites in New-Delhi', link: '/'},
+            {name: 'Projects in New-Delhi', link: '/'},
+            {name: 'Investment Hotspot', link: '/'},
+            {name: 'Find an Agent', link: '/'},
+            {name: 'Home Interiors in Ne...', sName: 'new', link: '/'},
+        ],
+    },
+    {
+        Head: 'Buying Tools',
+        sublink: [
+            {name: 'Prop Worth', link: '/'},
+            {name: 'Rates & Trends', link: '/'},
+            {name: 'Buy vs Rent', link: '/'},
+            {name: 'Tips and Guides', link: '/'},
+        ],
+    },
+];
+
+export const rent_options = [
+    {
+        Head: 'Popular Choices',
+        sublink: [
+            {name: 'Owner Properties', link: '/'},
+            {name: 'Verified Properties', link: '/'},
+            {name: 'Furnished Homes', link: '/'},
+            {name: 'Bachelor Friendly Ho...', link: '/'},
+            {name: 'Immediately Available', link: '/'},
+        ],
+    },
+    {
+        Head: 'Properties Types',
+        sublink: [
+            {name: 'Flat for rent in New-Delhi', link: '/'},
+            {name: 'House for rent in New...', link: '/'},
+            {name: 'Villa for rent in New-D...', link: '/'},
+            {name: 'PG in New-Delhi', link: '/'},
+            {name: 'Office Space in New-...', link: '/'},
+            {name: 'Commercial Space in', link: '/'},
+            {name: 'Coworking Space in New...', link: '/'},
+            {name: 'Coliving Space in New...', link: '/'},
+            {name: 'Student Hostels in New...', link: '/'},
+            {name: 'Luxury PG in New-Del...', link: '/'},
+        ],
+    },
+    {
+        Head: 'Budget',
+        sublink: [
+            {name: 'Under ₹ 10,000', link: '/'},
+            {name: '₹ 10,000 - ₹ 15,000', link: '/'},
+            {name: '₹ 15,000 - ₹ 25,000', link: '/'},
+            {name: 'Above ₹ 25,000', link: '/'},
+        ],
+    },
+    {
+        Head: 'Explore',
+        sublink: [
+            {name: 'Localities', link: '/'},
+            {name: 'Buy vs rent', link: '/'},
+            {name: 'Find an Agent', link: '/'},
+            {name: 'Share Requirement', link: '/'},
+            {name: 'Properties Services', link: '/'},            
+            {name: 'Rent Agreement', link: '/'},
+        ],
+    },
+    {
+        Head: 'Buying Tools',
+        sublink: [
+            {name: 'Prop Worth', link: '/'},
+            {name: 'Rates & Trends', link: '/'},
+            {name: 'Buy vs Rent', link: '/'},
+            {name: 'Tips and Guides', link: '/'},
+        ],
+    },
+];
+
+export const sell_options = [
+    {
+        Head: 'For Agent & Builder',
+        sublink: [
+            {name: 'My Dashboard', link: '/'},
+            {name: 'Ad Packages', link: '/'},
+            {name: 'Advantage', link: '/'},
+            {name: 'Developer Lounge', link: '/'},
+            {name: 'Sales Enquiry', link: '/'},
+        ],
+    },
+    {
+        Head: 'Selling Tools',
+        sublink: [
+            {name: 'Property Evaluation', link: '/'},
+            {name: 'Find an Agent', link: '/'},
+            {name: 'Rates & Trends', link: '/'},
+            {name: 'Prop Worth', link: '/'},
+        ],
+    },
+];
+
+export const homeLoan_options = [
+    {
+        Head: 'Apply Now',
+        sublink: [
+            {name: 'Home Loans', link: '/'},
+            {name: 'Balance Transfer', link: '/'},
+            {name: 'Loan Against', link: '/'},
+            {name: 'Property', link: '/'},
+        ],
+    },
+    {
+        Head: 'Partners',
+        sublink: [
+            {name: 'SBI Home Loan', link: '/'},
+            {name: 'HDFC Ltd Home', link: '/'},
+            {name: 'Loan', link: '/'},
+            {name: 'Axis Home Loan', link: '/'},
+            {name: 'Kotak Home Loan', link: '/'},
+            {name: 'LIC HF Home Loan', link: '/'},
+        ],
+    },
+    {
+        Head: 'Explore',
+        sublink: [
+            {name: 'Home Loan EMI Calculator', link: '/'},
+            {name: 'Home EMI Calculator', link: '/'},
+            {name: 'Get Home Loan Offers', sName: 'new', link: '/'},
+            {name: 'Check Credit Score', link: '/'},
+            {name: 'Home Loan Repayment'},
+            {name: 'Home Loan Interest Rate', link: '/'},
+            {name: 'Home Loan Balance', link: '/'},
+            {name: 'Transfer', link: '/'},
+            {name: 'Home Loan Documentation', link: '/'},
+        ],
+    },
+    {
+        Head: 'EMI Calculator',
+        sublink: [
+            {name: 'SBI Home Loan EMI Calculator', link: '/'},
+            {name: 'HDFC Home EMI Calculator', link: '/'},
+            {name: 'Axis Bank Home Loan EMI Calculator', link: '/'},
+            {name: 'Calculator', link: '/'},
+            {name: 'Bajaj Home Loan EMI Calculator'},
+            {name: 'Kotak Home Loan EMI Calculator', link: '/'},
+            {name: 'L&T Home Loan EMI Calculator', link: '/'},
+        ],
+    },
+    {
+        Head: 'Interest Rates',
+        sublink: [
+            {name: 'SBI Home Loan Interest Rate', link: '/'},
+            {name: 'HDFC Home Interest Rate', link: '/'},
+            {name: 'Axis Bank Home Loan Interest Rate', link: '/'},
+            {name: 'Rate', link: '/'},
+            {name: 'Bajaj Home Loan Interest Rate'},
+            {name: 'Kotak Home Loan Interest Rate', link: '/'},
+            {name: 'L&T Home Loan Interest Rate', link: '/'},
+        ],
+    },
+];
+
+export const homeInterior_options = [
+    {
+        Head: 'Home Interior Design Ideas',
+        sublink: [
+            {name: 'Modular Integer Design Idea', link: '/'},
+            {name: 'False Ceiling Design Ideas', link: '/'},
+            {name: 'Bathroom Design Ideas', link: '/'},
+            {name: 'Bedroom Design Ideas', link: '/'},
+            {name: 'TV Unit Design Ideas', link: '/'},
+        ],
+    },
+    {
+        Head: 'Full Home Interior Designs',
+        sublink: [
+            {name: '1BHK Interior Design', link: '/'},
+            {name: '2BHK Interior Design', link: '/'},
+            {name: '3BHK Interior Design', link: '/'},
+            {name: '4BHK Interior Design', link: '/'},
+        ],
+    },
+    {
+        Head: 'Explore Our Services',
+        sublink: [
+            {name: 'Home Interior Design Services', link: '/'},
+            {name: 'Interior Designers in New-Delhi', link: '/'},
+            {name: 'Design Consultation', sName: 'new', link: '/'},
+            {name: 'Full Home Interior Cost Calculator', link: '/'},
+            {name: 'Kitchen / Wardrobe Calculator'},
+        ],
+    },
+];
+
+export const advice_options = [
+    {
+        Head: 'MB Research',
+        sublink: [
+            {name: 'Insights', link: '/'},
+            {name: 'Research', link: '/'},
+            {name: 'Prop Index', link: '/'},
+            {name: 'Find Pincode', link: '/'},
+        ],
+    },
+    {
+        Head: 'Services & Tools',
+        sublink: [
+            {name: 'Property Valuation', sName: 'new', link: '/'},
+            {name: 'Rates & Trends', link: '/'},
+            {name: 'Area Converter', link: '/'},
+            {name: 'PropWorth', link: '/'},
+            {name: 'Buy v/s Rent', link: '/'},
+        ],
+    },
+    {
+        Head: 'Localities & Projects',
+        sublink: [
+            {name: 'Localities in New-Delhi', link: '/'},
+            {name: 'Locality Review Videos', link: '/'},
+            {name: 'Compare Localities', link: '/'},
+            {name: 'New Projects in New-De...', link: '/'},
+            {name: 'Project Review Videos', link: '/'},
+            {name: 'MBTV Videos', link: '/'},
+        ],
+    },
+    {
+        Head: 'News & Blogs',
+        sublink: [
+            {name: 'Latest Blogs', sName: 'new', link: '/'},
+            {name: 'Lifestyle', link: '/'},
+            {name: 'Policies Localities', link: '/'},
+            {name: 'Finance & Legal', link: '/'},
+            {name: 'City Blogs', link: '/'},
+            {name: 'Property News', link: '/'},
+            {name: 'Trending Web Stories', link: '/'},
+        ],
+    },
+];
+
+export const help_options = [
+    {name: 'Help Center', link: '/'},
+    {name: 'Sales Enquiry', link: '/'},
+]
